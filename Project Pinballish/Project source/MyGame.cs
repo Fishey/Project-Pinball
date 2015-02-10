d2344c263a0efe148c61c94ce986ab5e4f395fca
Fishey <randybrussen@gmail.com>
2015-02-10 17:20:10 +0100

    Added sounds and proper working projectiles.

--- Project Pinballish/Project source/MyGame.cs
+++ Project Pinballish/Project source/MyGame.cs
@@ -11,16 +11,18 @@ namespace GXPEngine
 		}
 
 		private List<Ship> _ships;
+		private List<Projectile> _projectiles;
 		private Ship _ship = null;
 		private Ship _ship2 = null;
+		private SoundChannel _pewpew;
+		private SoundChannel _pewpew2;
 		private Vec2 _center = null;
-
 
 		public MyGame ():base (1280,720, false) {
 
 			AddChild(new Sprite("background.jpg")); // add a beautiful background
 			_ships = new List<Ship> (); // create the list for ships (Player 1 & 2 go here)
-
+			_projectiles = new List<Projectile> ();
 			//Create ships
 			_ship = new Ship(25, 1);
 			_ship.position.x = width/2 + width/4;
@@ -30,7 +32,7 @@ namespace GXPEngine
 			_ship2 = new Ship (25, 2);
 			_ship2.position.x = width / 2 - width / 4;
 			_ship2.position.y = height / 2 - 50;
-			//_ships.Add (_ship2); //uncomment this to add a second ship
+			_ships.Add (_ship2); //uncomment this to add a second ship
 
 			foreach(Ship ship in _ships)
 				AddChild (ship); // add the ships to the game
@@ -44,18 +46,47 @@ namespace GXPEngine
 				processInput (ship);
 				ship.Step ();
 			}
+			foreach (Projectile bullet in _projectiles) {
+				bullet.Step ();
+			}
 		}
 
 		void processInput(Ship ship)
 		{
 			switch (ship.PlayNum) {
 			case 1:
-				if (Input.GetKey (Key.UP)) {
+				if (Input.GetKeyDown (Key.UP)) {
 					Console.WriteLine ("pew pew!");
 					Projectile bullet = new Projectile (10);
+					bullet.position = ship.position.Clone();
 					bullet.velocity = new Vec2 (10, 0);
 					bullet.velocity.SetAngleDegrees (ship.rotation);
-
+					bullet.rotation = ship.rotation;
+					_projectiles.Add (bullet);
+					AddChild (bullet);
+					if (_pewpew != null) {
+						_pewpew.Stop ();
+					}
+					Sound pewpew = new Sound ("laser1.wav");
+					_pewpew = pewpew.Play ();
+					
+				}
+				break;
+			case 2:
+				if (Input.GetKeyDown (Key.W)) {
+					Console.WriteLine ("pew pew!");
+					Projectile bullet = new Projectile (10);
+					bullet.position = ship.position.Clone();
+					bullet.velocity = new Vec2 (10, 0);
+					bullet.velocity.SetAngleDegrees (ship.rotation);
+					bullet.rotation = ship.rotation;
+					_projectiles.Add (bullet);
+					AddChild (bullet);
+					if (_pewpew2 != null) {
+						_pewpew2.Stop ();
+					}
+					Sound pewpew = new Sound ("laser2.wav");
+					_pewpew2 = pewpew.Play ();
 				}
 				break;
 			default:
@@ -71,6 +102,7 @@ namespace GXPEngine
 
 			double cosAngle = Math.Cos (angle/5);
 			double sinAngle = Math.Sin (angle/5);
+			// player1 controls
 			if (Input.GetKey(Key.RIGHT) && sprite.PlayNum == 1)
 			{
 				sprite.position.x = (float)(center.x + dx * cosAngle - dy * sinAngle);
@@ -84,6 +116,21 @@ namespace GXPEngine
 				sprite.position.x = (float)(center.x + dx * cosAngle - dy * sinAngle);
 				sprite.position.y = (float)(center.y + dx * sinAngle + dy * cosAngle);
 			}
+			//player2 controls
+			if (Input.GetKey(Key.D) && sprite.PlayNum == 2)
+			{
+				sprite.position.x = (float)(center.x + dx * cosAngle - dy * sinAngle);
+				sprite.position.y = (float)(center.y + dx * sinAngle + dy * cosAngle);
+			}
+			else if (Input.GetKey(Key.A) && sprite.PlayNum == 2)
+			{
+				cosAngle = Math.Cos (-angle / 5);
+				sinAngle = Math.Sin (-angle / 5);
+
+				sprite.position.x = (float)(center.x + dx * cosAngle - dy * sinAngle);
+				sprite.position.y = (float)(center.y + dx * sinAngle + dy * cosAngle);
+			}
+
 			sprite.rotation = (float)Math.Atan2 (dy, dx) * 180 / (float)Math.PI - 180;
 		}
 
